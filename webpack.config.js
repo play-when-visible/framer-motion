79508efb3ef0d77d961085efaf3ccb4023386493
Im-Stevemmmmm <estebangarcia2121@gmail.com>
2020-12-18 16:38:53 -0800

    Updated app path

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+/* eslint-disable @typescript-eslint/no-var-requires */
 const path = require("path");
 
 module.exports = {
     entry: {
-        app: path.resolve(__dirname, "src/index.tsx"),
+        app: path.resolve(__dirname, "src/play-when-visible.tsx"),
     },
     output: {
-        path: path.resolve(__dirname, "dist"),
+        path: path.resolve(__dirname, "/dist"),
         filename: "main-bundle.js",
     },
     module: {
